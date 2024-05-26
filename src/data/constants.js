4d82845b127a96980ff93a1d6831c5428ef33d5a
Vraj-Raval <ravalvraj3000@gmail.com>
2024-05-26 19:12:26 +0530

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,254 @@
+export const Bio = {
+  name: "Vraj Raval",
+  roles: [
+    "Full Stack Developer",
+    "UI/UX Designer",
+    "Android Developer",
+    "Programmer",
+  ],
+  description:
+    "I am a dynamic professional with a passion for learning and excellence. Eager to embrace new challenges, I deliver high-quality results with a positive attitude and growth mindset. Ready to make impactful contributions and achieve great things.",
+  github: "https://github.com/Vraj-Raval",
+  resume:
+    "https://drive.google.com/file/d/1Fn9zHMdMnW3e78arJk4vSVZn1EtQ6oDO/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/vraj-raval-54790b231/",
+  twitter: "https://x.com/VrajRaval_",
+  insta: "https://www.instagram.com/vrajrvl/",
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/3/3d/CSS.3.svg",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Tailwind CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/d/d5/Tailwind_CSS_Logo.svg",
+      },
+      {
+        name: "Flutter",
+        image:
+          "https://cdn-images-1.medium.com/max/1200/1*5-aoK8IBmXve5whBQM90GA.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "https://www.svgrepo.com/show/378837/node.svg",
+      },
+      {
+        name: "Express Js",
+        image:
+          "https://www.svgrepo.com/show/330398/express.svg",
+      },
+      {
+        name: "SQL Server Management Studio",
+        image:
+          "https://www.svgrepo.com/show/374093/sql.svg",
+      },
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+      {
+        name: "Firebase",
+        image: "https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg",
+      },
+    ],
+  },
+  {
+    title: "Android",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "Kotlin",
+        image:
+          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+      },
+      {
+        name: "Android Studio",
+        image:
+          "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/6/62/Git-logo-orange.svg",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://www.svgrepo.com/show/452129/vs-code.svg",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://www.svgrepo.com/show/354202/postman-icon.svg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://www.svgrepo.com/show/452202/figma.svg",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C4E0BAQGs7EmZ0yQMpQ/company-logo_200_200/0/1661493099915?e=1724284800&v=beta&t=uYL9tM4_8jDr9rSWTi2Ah_bWsC8obi9sb5pJKHL9uaI",
+    role: "Web Developer Intern",
+    company: "Internpe Pvt. Ltd.",
+    date: "Sep 2023 - Dec 2023",
+    desc: "Created Web-based projects (Ecommerce Website, TO-DO list), actively contributing to company operations.",
+    skills: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "JavaScript"
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/portfolio-2baae.appspot.com/o/VRAJ%20RAMESHBHAI%20RAVAL%20Completion%20Certificate.png?alt=media&token=4e3c05c5-ba3a-4c16-ac33-7b92b29cdf45",
+  },
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-2baae.appspot.com/o/Screenshot%202024-05-26%20122842.png?alt=media&token=d4e9a528-e6c2-4dce-bb66-37b5a4bad4a1",
+    role: "Teaching Assistantship in Python",
+    company: "Darshan University",
+    date: "2023",
+    desc: "As a Python Teaching Assistant, I assist with coursework, provide lab guidance, offer one-on-one tutoring, and help students understand and master Python programming.",
+    
+    
+  }
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://firebasestorage.googleapis.com/v0/b/portfolio-2baae.appspot.com/o/Screenshot%202024-05-26%20122842.png?alt=media&token=d4e9a528-e6c2-4dce-bb66-37b5a4bad4a1",
+    school: "Darshan University, Rajkot",
+    date: "2021 - 2025",
+    grade: "7.51 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Darshan University, Rajkot. I have completed 6 semesters and have a CGPA of 7.51. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree: "Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://scontent.fraj1-1.fna.fbcdn.net/v/t39.30808-1/300434269_558443459407649_3341149347748459891_n.png?stp=dst-png_p200x200&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_ohc=-GOc1YZb5boQ7kNvgGilYHp&_nc_ht=scontent.fraj1-1.fna&oh=00_AYCQhZo2nbFRHoEVtc7HXGYGRFlGbbEtq0G8fTLDpAPCkA&oe=6653567F",
+    school: "Modi School, Rajkot",
+    date: "2020 - 2021",
+    grade: "77.69%",
+    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer subject.",
+    degree: "XII (HSC-GSEB)",
+  },
+  {
+    id: 2,
+    img: "https://scontent.fraj1-1.fna.fbcdn.net/v/t39.30808-1/302078145_490295826437978_7373990499362775230_n.jpg?stp=dst-jpg_p200x200&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_ohc=gKcUemmXfC4Q7kNvgEOUmys&_nc_ht=scontent.fraj1-1.fna&oh=00_AYCJ_MyD9pwcY5gD_P6VigW8h3UV-flzA6lWHwSvdR7dAQ&oe=66539015",
+    school: "K.G. Dholakiya School, Rajkot",
+    date: "2018 - 2019",
+    grade: "77.50%",
+    desc: "I completed my class 10 education at K.G. Dholakiya School, Rajkot.",
+    degree: "X (SSC-GSEB)",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Go Green Way",
+    date: "Feb 2024 - Apr 2024",
+    description:
+      "Developed an innovative e-commerce platform focused on plant enthusiasts,Engineered a seamless buying and selling experience for plants,Enabled users to navigate effortlessly, fostering a personalized and user-friendly online gardening journey.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-2baae.appspot.com/o/screencapture-localhost-3002-Plant-2024-05-22-18_08_56.png?alt=media&token=3532eb83-50c4-45a5-ab93-a62ff4232d95",
+    tags: [
+      "React Js",
+      "Node Js",
+      "MongoDb",
+      "Express Js",
+      "JWT Token",
+    ],
+    category: "web app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 3,
+    title: "ShoeSnap",
+    date: "Jul 2023 - Aug 2023",
+    description:
+      "Created a premier e-commerce platform dedicated to footwear fans,Designed and implemented a trendy and user-friendly interface.",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-2baae.appspot.com/o/NWAJDtaovOU-HD.jpg?alt=media&token=5ea41df8-b9c1-43aa-a02b-cd6d8612d049",
+    tags: ["Kotlin","MongoDb"],
+    category: "android app",
+    github: "",
+    webapp: "",
+  },
+  {
+    id: 3,
+    title: "Plant Ecommerce",
+    date: "Feb 2023 - Mar 2023",
+    description:
+      "Introducing the ultimate app for anyone looking to bring a touch of nature into their homes and offices. With our app, you can browse and buy a wide range of house and office plants, including succulents, cacti, ferns, and more!",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-2baae.appspot.com/o/Frame%201.png?alt=media&token=4ae10d6f-0c0f-437b-842d-64a56343bd00",
+    tags: ["Figma"],
+    category: "ui design",
+    github: "https://www.figma.com/design/AoQFee1FIaiosbO54CKGTq/Plant-Ecommerc?m=dev&node-id=0%3A1&t=5W6uirQGneSbxa0u-1",
+    webapp: "https://www.figma.com/design/AoQFee1FIaiosbO54CKGTq/Plant-Ecommerc?m=dev&node-id=0%3A1&t=5W6uirQGneSbxa0u-1",
+  },
+  
+  
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
